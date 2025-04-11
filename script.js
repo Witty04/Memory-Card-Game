fa774dabde5917a43ada010ee953b2260a0af7f4
Witty04 <dwikumeilani@gmail.com>
2025-04-11 13:07:04 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,90 @@
+const cards = [
+    '🥨', '🥨',
+    '🍰', '🍰',
+    '🥗', '🥗',
+    '🥫',  '🥫',
+    '🍫',  '🍫',
+    '🍺',  '🍺',
+    '🍷',  '🍷',
+    '🧊', '🧊'
+];
+
+let firstCard = null;
+let secondCard = null;
+let lockboard = false;
+
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j =
+        Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+function createBoard() {
+    const gameBoard = document.querySelector('.game-board');
+    shuffle(cards);
+    cards.forEach(card => {
+        const cardElement = document.createElement('div');
+
+        cardElement.classList.add('card');
+            cardElement.dataset.icon = card;
+
+        cardElement.addEventListener('click', flipCard);
+
+        gameBoard.appendChild(cardElement);  
+    });
+}
+
+function flipCard() {
+    if (lockboard) return;
+    if (this === firstCard) return;
+
+    this.classList.add('flipped')
+    this.textContent = this.dataset.icon;
+
+    if (!firstCard) {
+        firstCard = this;
+        return;
+    }
+
+    secondCard = this;
+    checkForMatch();
+}
+
+function checkForMatch() {
+    if (firstCard.dataset.icon === secondCard.dataset.icon) {
+        disableCards();
+    } else {
+        unflipCards();
+    }
+}
+
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+
+    secondCard.removeEventListener('click', flipCard);
+        resetBoard();
+}
+
+function unflipCards() {
+    lockboard = true;
+    setTimeout(() => {
+        firstCard.classList.remove('flipped');
+        secondCard.classList.remove('flipprd');
+            firstCard.textContent = '';
+            secondCard.textContent = '';
+            resetBoard();
+    }, 1000);
+}
+
+function resetBoard() {
+    [firstCard, secondCard, lockboard] = [null, null, false];
+}
+
+document.getElementById('reset-button').addEventListener('click', () => {
+    document.querySelector('.game-board').innerHTML = '';
+    createBoard();
+})
+
+createBoard();
